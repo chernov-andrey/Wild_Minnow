992b3205976ae4cd3dc8c17d10544b75d33c75d7
Chernov Andrey <xWmaximusWx@yandex.ru>
2026-04-18 18:21:32 +0300

    Create project.

---
+++ Wild_Minnow/Source/Wild_Minnow/Wild_Minnow.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Wild_Minnow.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Wild_Minnow, "Wild_Minnow" );
